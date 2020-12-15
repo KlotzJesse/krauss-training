8adb5c1c85ab78151d9897c073a84cb5fa8af19c
Jesse Klotz <j.klotz@student.xu-university.de>
2020-12-15 22:41:03 +0100

    Initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,14 +8,41 @@ module.exports = {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "asd",
+        anonymize: true,
       },
     },
     "gatsby-plugin-sharp",
+    `gatsby-plugin-advanced-sitemap`,
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     "gatsby-plugin-offline",
-    "gatsby-plugin-manifest",
     "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`, `700`]
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `KRAUSS Training`,
+        short_name: `KRAUSSTraining`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
