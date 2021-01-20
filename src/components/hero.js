1591b792fbcd73ccb36a88e28935c86ee09e7c5b
Jesse Klotz <jesseklotz2306@gmail.com>
2021-01-20 16:25:10 +0100

    Add fadein animation

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,7 @@ import tw from "twin.macro";
 export default function Hero({ children }) {
   return (
     <section class="text-gray-700 flex-grow flex flex-col xl:flex-row xl:space-x-32 space-y-5 md:space-y-0 mx-auto items-center justify-center xl:py-0 py-28">
-      <div class="flex-initial w-2/3 flex flex-col items-center justify-center">
+      <div class="flex-initial w-2/3 flex flex-col items-center justify-center animate-fadein">
         <h1 class="font-serif text-2xl font-semibold text-gray-800 dark:text-white xl:text-5xl xl:leading-tight leading-tight mb-8">
           Praxisnahe & hoch beeindruckende Trainings für Führungskräfte &
           Verkäufer
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
-  theme: {},
+  theme: {
+    extend: {
+      keyframes: {
+       fadein: {
+         '0%': { opacity: 0 },
+         '100%': { opacity: 1 },
+       }
+      },
+      animation: {
+        fadein: 'fadein 1s',
+       }
+    } 
+  },
   variants: {},
   plugins: [],
   darkMode: "class",
